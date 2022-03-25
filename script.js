0c64397d51305340ab9f2a8a08a77d69e04d3a7d
dashakozachenko <dkozacenko4@gmail.com>
2022-03-25 11:40:20 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+//Task# 1
+
+//const result = Boolean(Number('10'))+1;
+//console.log(result, typeof result);
+
+//const result = `sub` + Number(false);
+//console.log(result, typeof result);
+
+//const result = 16 * '      91    ';
+//console.log(result, typeof result);
+
+//const result = true - 70;
+//console.log(result, typeof result);
+
+//const result = Number(1 + String(1))+1;
+//console.log(result, typeof result);
+
+//Task# 2
+
+//const secondsInHours = 3600 * +prompt("Enter the number of hours to translate into seconds:");
+//alert (`Result: ${secondsInHours} seconds `);
+
+//Task# 3
+
+let num = 1
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
